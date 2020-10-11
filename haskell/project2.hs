71c6f8c9142168faa1240a11aa09d658ae30f5e2
extragravee <singhsidak@ymail.com>
2020-10-12 01:16:14 +1100

    yeetus

--- haskell/project2.hs
+++ haskell/project2.hs
@@ -20,3 +20,26 @@ toPitch str
     where pitch = Pitch note octave
           note  = str !! 0
           octave= str !! 1
+
+-------------------------------------------
+-- first is target, next is guess
+
+feedback :: [Pitch] -> [Pitch] -> (Int, Int, Int)
+feedback target guess = (pitches, notes, octaves)
+                     where pitches = countMatches target guess --change to remove matched pitch
+                           notes   = countMatches (map noteFromPitch target) (map noteFromPitch guess)
+                           octaves = countMatches (map octFromPitch target) (map octFromPitch guess)
+--countPitches [Pitch 'A' '1',Pitch 'B' '2',Pitch 'A' '3'] [Pitch 'A' '1',Pitch 'A' '2',Pitch 'B' '1']
+
+noteFromPitch:: Pitch -> Char
+noteFromPitch (Pitch x _) = x
+
+octFromPitch:: Pitch -> Char
+octFromPitch (Pitch _ x) = x
+
+
+countMatches:: Eq a => [a] -> [a] -> Int
+countMatches _ [] = 0
+countMatches target (x:xs)
+    | elem x target = 1 + countMatches target xs
+    | otherwise     = countMatches target xs
